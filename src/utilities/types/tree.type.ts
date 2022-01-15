8582798b5587516eeb99a066fbfb5e5c5ed27c4d
Milan Štiglic <stiglic.work@gmail.com>
2022-01-15 19:28:28 +0100

    Refactored types to interfaces

--- src/utilities/types/tree.type.ts
+++ src/utilities/types/tree.type.ts
@@ -1,28 +1,28 @@
-export type TreeItemType = {
+export interface TreeItemType {
   parent: number | null;
   id: number;
   name: string;
   children: TreeItemType[] | [];
   type: "file" | "folder";
-};
+}
 
-export type FlatItemType = {
+export interface FlatItemType {
   parent: number | null;
   id: number;
   name: string;
   type: "file" | "folder";
-};
+}
 
-export type TreeCallbackItemType = {
+export interface TreeCallbackItemType {
   parent: number | null;
   id: number;
   name: string;
   children: boolean;
   type: "file" | "folder";
-};
+}
 
-export type CachedTreeType = {
+export interface CachedTreeType {
   flat: FlatItemType[];
   tree: TreeItemType[];
   selected: TreeCallbackItemType;
-};
+}
