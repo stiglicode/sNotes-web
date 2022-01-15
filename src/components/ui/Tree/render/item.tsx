42f396f32251e21d018d17b59321a4691020f43f
Milan Štiglic <stiglic.work@gmail.com>
2022-01-15 19:28:06 +0100

    Fixed call back state issue

--- src/components/ui/Tree/render/item.tsx
+++ src/components/ui/Tree/render/item.tsx
@@ -1,45 +1,57 @@
-import React, { useState } from "react";
+import React, { FC, useState } from "react";
 import { TreeCallbackItemType, TreeItemType } from "../../../../utilities/types/tree.type";
 import { TreeItem } from "@mui/lab";
 import { Delete, DisabledByDefault, Edit, Folder, InsertDriveFile } from "@mui/icons-material";
 
-const RenderItem = (data: TreeItemType[], onSelect: (callBack: TreeCallbackItemType) => any) => {
+interface ItemProps {
+  node: TreeItemType;
+  index: number;
+  onSelect: (callBack: TreeCallbackItemType) => void;
+  self: (node: TreeItemType[]) => JSX.Element[];
+}
+
+const Item: FC<ItemProps> = ({ node, index, onSelect, self }) => {
+  const [editable, setEditable] = useState(true);
+
+  return (
+    <TreeItem
+      key={index}
+      nodeId={String(node.id)}
+      onClick={() => {
+        return onSelect({
+          id: node.id,
+          parent: node.parent,
+          type: node.type,
+          name: node.name,
+          children: !!node.children.length,
+        });
+      }}
+      label={
+        <div className={"tree-rendered-item"}>
+          <div className={"tree-rendered-item_box left"}>
+            {node.type === "file" ? <InsertDriveFile /> : node.type === "folder" ? <Folder /> : <></>}
+            <input defaultValue={node.name} disabled={editable} type={"text"} />
+          </div>
+          <div className={"tree-rendered-item_box right"}>
+            <label className={"pop-up"} onClick={() => setEditable(!editable)}>
+              {!editable ? <DisabledByDefault /> : <Edit />}
+            </label>
+            <label className={"delete pop-up"}>
+              <Delete />
+            </label>
+          </div>
+        </div>
+      }
+    >
+      {self(node?.children)}
+    </TreeItem>
+  );
+};
+
+const RenderItem = (data: TreeItemType[], onSelect: (callBack: TreeCallbackItemType) => void) => {
   const render = (node: TreeItemType[]) => {
     return node?.map((rootNode, index) => {
-      const [editable, setEditable] = useState(true);
-      return (
-        <TreeItem
-          key={index}
-          nodeId={String(rootNode.id)}
-          onClick={() => {
-            return onSelect({
-              id: rootNode.id,
-              parent: rootNode.parent,
-              type: rootNode.type,
-              name: rootNode.name,
-              children: !!rootNode.children.length,
-            });
-          }}
-          label={
-            <div className={"tree-rendered-item"}>
-              <div className={"tree-rendered-item_box left"}>
-                {rootNode.type === "file" ? <InsertDriveFile /> : rootNode.type === "folder" ? <Folder /> : <></>}
-                <input defaultValue={rootNode.name} disabled={editable} type={"text"} />
-              </div>
-              <div className={"tree-rendered-item_box right"}>
-                <label className={"pop-up"} onClick={() => setEditable(!editable)}>
-                  {!editable ? <DisabledByDefault /> : <Edit />}
-                </label>
-                <label className={"delete pop-up"}>
-                  <Delete />
-                </label>
-              </div>
-            </div>
-          }
-        >
-          {render(rootNode?.children)}
-        </TreeItem>
-      );
+      return <Item key={index} node={rootNode} index={index} onSelect={onSelect} self={render} />;
     });
   };
   return render(data);
