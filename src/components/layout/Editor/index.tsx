eeaef0e91c4139dbe9b9ecd926973f34ff537f80
Milan Štiglic <stiglic.work@gmail.com>
2022-01-29 23:05:03 +0100

    Removed draft.js

--- src/components/layout/Editor/index.tsx
+++ src/components/layout/Editor/index.tsx
@@ -1,22 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { convertToRaw, EditorState } from "draft-js";
+import React from "react";
+// import { Editor } from "react-draft-wysiwyg";
+// import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+// import { convertToRaw, EditorState } from "draft-js";
 
 const RichTextEditor = () => {
-  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+  // const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  useEffect(() => {}, [editorState]);
+  // useEffect(() => {}, [editorState]);
 
   const onChange = async (state: any) => {
-    await setEditorState(state);
-    const data = convertToRaw(editorState.getCurrentContent());
+    // await setEditorState(state);
+    // const data = convertToRaw(editorState.getCurrentContent());
   };
-  return (
-    <div className={"editor-canvas"}>
-      <Editor onEditorStateChange={onChange} />
-    </div>
-  );
+  return <div className={"editor-canvas"}>a{/*<Editor onEditorStateChange={onChange} />*/}</div>;
 };
 
 export default RichTextEditor;
